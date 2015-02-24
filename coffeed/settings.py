59f11700bbdaa8243efe6015452d12242335c236
Eric Neuman <ericneuman@One-Months-Mac-mini.local>
2015-02-24 16:24:31 -0500

    updated settings for heroku

--- coffeed/settings.py
+++ coffeed/settings.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -56,6 +56,12 @@ WSGI_APPLICATION = 'coffeed.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
