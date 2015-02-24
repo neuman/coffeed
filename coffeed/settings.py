33631d5e1878e56a32a80e0a7558e1ddbe7d15f0
Eric Neuman <ericneuman@One-Months-Mac-mini.local>
2015-02-24 16:02:24 -0500

    set up for hreoku

--- coffeed/settings.py
+++ coffeed/settings.py
@@ -56,12 +56,16 @@ WSGI_APPLICATION = 'coffeed.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
